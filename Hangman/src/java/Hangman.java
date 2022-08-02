45ad38bd57b4f4ffdb3eebc3380b073e80bbbb12
Kaymon McCain <kaymonCodeM@outlook.com>
2022-08-02 16:00:53 -0500

    Created library and started the playHangman method

--- Hangman/src/java/Hangman.java
+++ Hangman/src/java/Hangman.java
@@ -1,4 +1,25 @@
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Hangman {
+
+    public InputStream in = System.in;
+    public String[] library = {"cat","dog","mouse","mop","stand","drive","smile","snark","shark","bat","ramp","ton","car","zoo"};
+
+    public void playHangman(){
+        Random rand = new Random();
+        int selectWord = rand.nextInt(library.length-1)+1;
+        String word = library[selectWord];
+        char[] userBank = new char[word.length()];
+        Arrays.fill(userBank, '_');
+        int userGuesses = 0;
+        System.out.println("  HANGMAN\n");
+        System.out.println(buildHangman(userGuesses));
+        char[] temp = userBank;
+        System.out.println(temp);
+
+    }
 
     public String buildHangman(int guesses){
         String top = "  +---+";
@@ -29,9 +50,10 @@ public class Hangman {
             default:
                 return top + "\n\n" + pole + "\n\n" + pole +"\n\n" + pole + "\n\n" +ground;
         }
+
     }
     public static void main(String[] args) {
         Hangman h = new Hangman();
-        System.out.println(h.buildHangman(1));
+        h.playHangman();
     }
 }
