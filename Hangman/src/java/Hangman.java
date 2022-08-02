45fddb988298917fa78f96cdd17fb724bf988b0d
Kaymon McCain <kaymonCodeM@outlook.com>
2022-08-02 15:06:54 -0500

    Created Full Hangman function

--- Hangman/src/java/Hangman.java
+++ Hangman/src/java/Hangman.java
@@ -5,20 +5,33 @@ public class Hangman {
         String pole = "      |";
         String ground = "     ===";
         String head = "  O   |";
-        String body = "  |   |";
+        String upperBody = "  |   |";
+        String lowerBody = "  |   |";
+        String leftLeg = " /";
+        String leftRightLeg = " / \\";
+        String leftArm = " \\";
+        String leftRightArm = " \\ /";
         switch (guesses){
-            case 0:
-                return top + "\n\n" + pole + "\n\n" + pole +"\n\n" + pole + "\n\n" +ground;
             case 1:
                 return top + "\n\n" + head + "\n\n" + pole +"\n\n" + pole + "\n\n" +ground;
             case 2:
-                return top + "\n\n" + head + "\n\n" + body +"\n\n" + pole + "\n\n" +ground;
-
+                return top + "\n\n" + head + "\n\n" + upperBody +"\n\n" + pole + "\n\n" +ground;
+            case 3:
+                return top + "\n\n" + head + "\n\n" + upperBody +"\n\n" + lowerBody + "\n\n" +ground;
+            case 4:
+                return top + "\n\n" + head + "\n\n" + upperBody +"\n\n" + lowerBody + "\n"+ leftLeg+"\n" +ground;
+            case 5:
+                return top + "\n\n" + head + "\n\n" + upperBody +"\n\n" + lowerBody + "\n"+ leftRightLeg+"\n" +ground;
+            case 6:
+                return top + "\n\n" + head + "\n"+ leftArm +"\n" + upperBody +"\n\n" + lowerBody + "\n"+ leftRightLeg+"\n" +ground;
+            case 7:
+                return top + "\n\n" + head + "\n"+ leftRightArm +"\n" + upperBody +"\n\n" + lowerBody + "\n"+ leftRightLeg+"\n" +ground;
+            default:
+                return top + "\n\n" + pole + "\n\n" + pole +"\n\n" + pole + "\n\n" +ground;
         }
-        return top;
     }
     public static void main(String[] args) {
         Hangman h = new Hangman();
-        System.out.println(h.buildHangman(2));
+        System.out.println(h.buildHangman(1));
     }
 }
--- Hangman/src/test/HangmanTest.java
+++ Hangman/src/test/HangmanTest.java
@@ -4,7 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HangmanTest {
 
+    Hangman hangman;
     @BeforeEach
     void setUp() {
+        hangman = new Hangman();
+    }
+
+    @DisplayName("Initiate Hangman")
+    @Test
+    void initiateHangman(){
+        String top = "  +---+";
+        String pole = "      |";
+        String ground = "     ===";
+        String result = top + "\n\n" + pole + "\n\n" + pole +"\n\n" + pole + "\n\n" +ground;
+        assertEquals(result,hangman.buildHangman(0),"Initiate Hangman Failed");
+    }
+    @DisplayName("Full Hangman")
+    @Test
+    void fullHangman(){
+        String top = "  +---+";
+        String pole = "      |";
+        String ground = "     ===";
+        String head = "  O   |";
+        String upperBody = "  |   |";
+        String lowerBody = "  |   |";
+        String leftLeg = " /";
+        String leftRightLeg = " / \\";
+        String leftArm = " \\";
+        String leftRightArm = " \\ /";
+        String result = top + "\n\n" + head + "\n"+ leftRightArm +"\n" + upperBody +"\n\n" + lowerBody + "\n"+ leftRightLeg+"\n" +ground;
+        assertEquals(result,hangman.buildHangman(7),"Full Hangman Failed");
     }
 }
